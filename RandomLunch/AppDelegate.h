f81b0eac7f0bb8bbc2fbf7dfadeb27dee5aee554
heaven <heaven12.7@gmail.com>
2013-08-09 08:31:24 +0900

    Project Init

---
+++ RandomLunch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RandomLunch
+//
+//  Created by Heaven on 13. 8. 9..
+//  Copyright (c) 2013년 Heaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RandomLunch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RandomLunch
+//
+//  Created by Heaven on 13. 8. 9..
+//  Copyright (c) 2013년 Heaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
