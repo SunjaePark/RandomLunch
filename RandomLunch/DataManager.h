c778c3131d3c45cda68f7a3aff402c6c8e4d6e46
Park Yoseop <yoseop.park86@gmail.com>
2013-08-20 20:40:31 +0900

    make DataManager
    세부적인 내용을 채움.
    - DB생성
    - 테이블 생성
    - 데이터 삽입
    - 데이터 가져오기

--- RandomLunch/DataManager.h
+++ RandomLunch/DataManager.h
@@ -1,24 +1,45 @@
 //
 //  DataManager.h
-//  Memo
+//  RandomLunch
 //
-//  Created by Heaven on 13. 8. 8..
+//  Created by Park Yoseop on 13. 8. 20..
 //  Copyright (c) 2013년 Heaven. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <sqlite3.h>
 
 @class Memo;
 
 @interface DataManager : NSObject
+{
+    sqlite3 *db;
+}
 
-+ (id)defaultManager;
+//데이터베이스의 전체 경로 반환
+- (NSString *) filePath;
 
-// 아래와 같은 형태로 각각을 입력 받도록 구성할 수도 있고
-// 아니면 - (void)insertWithDictionary:(NSDictionary)dictionary 와 같이 범용적으로 사용할 수 있도록 구성 할 수도 있을 것 같습니다.
-// 이외의 다른 형태로도 고민해 보시면 좋을 것 같네요.
-- (void)insertWithTitle:(NSString *)title memo:(NSString *)memo;
-- (void)updateWithTitle:(NSString *)title memo:(NSString *)memo where:(int)indexNumber;
-- (NSArray *)memoList;
+//데이터베이스 생성
+- (void) openDB;
+
+//테이블 생성
+- (void) createTableNamed:(NSString *) tableName withField1:(NSString *) field1 withField2:(NSString *) field2 withField3:(NSString *) field3;
+
+//레코드 삽입
+- (void) insertRecordIntoTableNamed:(NSString *) tableName withField1:(NSString *) field1 field2:(NSString *) field2 field3:(NSString *) field3 field1Value:(NSString *) field1Value field2Value:(NSString *) field2Value field3Value:(NSString *) field3Value;
+
+//레코드 가져오기
+- (void) getAllRowsFromTableNamed: (NSString *) tableName;
+
+
+//+ (id)defaultManager;
+//
+//// 아래와 같은 형태로 각각을 입력 받도록 구성할 수도 있고
+//// 아니면 - (void)insertWithDictionary:(NSDictionary)dictionary 와 같이 범용적으로 사용할 수 있도록 구성 할 수도 있을 것 같습니다.
+//// 이외의 다른 형태로도 고민해 보시면 좋을 것 같네요.
+//
+//- (void)insertWithTitle:(NSString *)title memo:(NSString *)memo;
+//- (void)updateWithTitle:(NSString *)title memo:(NSString *)memo where:(int)indexNumber;
+//- (NSArray *)memoList;
 
 @end
---
+++ RandomLunch/DataManager.m
@@ -0,0 +1,82 @@
+//
+//  DataManager.m
+//  RandomLunch
+//
+//  Created by Park Yoseop on 13. 8. 20..
+//  Copyright (c) 2013년 Heaven. All rights reserved.
+//
+
+#import "DataManager.h"
+#import <sqlite3.h>
+
+@implementation DataManager
+
+- (NSString *) filePath
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(
+                                                         NSDocumentationDirectory, NSUserDomainMask, YES);
+    NSString *documentsDir = [paths objectAtIndex:0];
+    return [documentsDir stringByAppendingPathComponent:@"database.sql"];
+}
+
+- (void) openDB
+{
+    //데이터베이스 생성
+    if (sqlite3_open([[self filePath] UTF8String], &db) != SQLITE_OK){
+        sqlite3_close(db);
+        NSAssert(0, @"Database failed to open.");
+    }
+}
+
+- (void) createTableNamed:(NSString *) tableName withField1:(NSString *) field1 withField2:(NSString *) field2 withField3:(NSString *) field3
+{
+    char *err;
+    //SQL문
+    NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXISTS '%@' ( '%@' TEXT PRIMARY KEY, '%@' TEXT, '%@' TEXT);", tableName, field1, field2, field3];
+    
+    if (sqlite3_exec(db, [sql UTF8String], NULL, NULL, &err) !=SQLITE_OK)
+    {
+        sqlite3_close(db);
+        NSAssert(0, @"Tabled failed to create.");
+    }
+}
+
+- (void) insertRecordIntoTableNamed:(NSString *) tableName withField1:(NSString *) field1 field2:(NSString *) field2 field3:(NSString *) field3 field1Value:(NSString *) field1Value field2Value:(NSString *) field2Value field3Value:(NSString *) field3Value
+{
+    //SQL문
+    NSString *sql = [NSString stringWithFormat:@"INSERT OR REPLACE INTO '%@' ('%@','%@','%@') VALUES ('%@','%@','%@')", tableName,field2, field2,field3,field1Value,field2Value,field3Value ];
+    
+    char *err;
+    if (sqlite3_exec(db, [sql UTF8String], NULL, NULL, &err) != SQLITE_OK)
+    {
+        sqlite3_close(db);
+        NSAssert(0, @"Error Updating Table.");
+    }
+}
+
+- (void) getAllRowsFromTableNamed: (NSString *) tableName
+{
+    //열가져오기.
+    NSString *qsql = [NSString stringWithFormat:@"SELECT * FROM %@",tableName];
+    
+    sqlite3_stmt *statement;
+    if (sqlite3_prepare_v2(db, [qsql UTF8String], -1, &statement, nil) == SQLITE_OK)
+    {
+        while(sqlite3_step(statement) == SQLITE_ROW)
+        {
+            char *field1 = (char *) sqlite3_column_text(statement, 0);
+            NSString *field1Str = [[NSString alloc] initWithUTF8String:field1];
+            char *field2 = (char *) sqlite3_column_text(statement, 0);
+            NSString *field2Str = [[NSString alloc] initWithUTF8String:field2];
+            char *field3 = (char *) sqlite3_column_text(statement, 0);
+            NSString *field3Str = [[NSString alloc] initWithUTF8String:field3];
+            
+            NSString *str = [[NSString alloc] initWithFormat:@" %@ - %@ - %@ ", field1Str, field2Str, field3Str];
+            NSLog(@"%@", str);
+        }
+        // 메모리에 쌓인 쿼리문 삭제
+        sqlite3_finalize(statement);
+    }
+}
+
+@end
